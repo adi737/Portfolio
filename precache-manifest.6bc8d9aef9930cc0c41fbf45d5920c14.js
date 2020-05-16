3115606a3e3a87c6edf68bdc24383bdd5a61ff42
Adrian Nidecki <adrian.nidecki@wp.pl>
2020-05-16 10:19:55 +0200

    Updates

---
+++ precache-manifest.6bc8d9aef9930cc0c41fbf45d5920c14.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cefc566ae9c586e9d2dcbc646abea7e",
+    "url": "/adi737/Portfolio.git/index.html"
+  },
+  {
+    "revision": "4122aa2841f623cc2c01",
+    "url": "/adi737/Portfolio.git/static/css/main.c2110752.chunk.css"
+  },
+  {
+    "revision": "69d208bf18ea65b633a3",
+    "url": "/adi737/Portfolio.git/static/js/2.9d6a07d9.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/adi737/Portfolio.git/static/js/2.9d6a07d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4122aa2841f623cc2c01",
+    "url": "/adi737/Portfolio.git/static/js/main.ad3b3ec0.chunk.js"
+  },
+  {
+    "revision": "14e1d5361dfb98d4870c",
+    "url": "/adi737/Portfolio.git/static/js/runtime-main.4934c047.js"
+  },
+  {
+    "revision": "6a6d60a0c8d58f10d55918f70e71bb95",
+    "url": "/adi737/Portfolio.git/static/media/code-1280.6a6d60a0.jpg"
+  },
+  {
+    "revision": "7f7ba52140f260e16089541f0d13e354",
+    "url": "/adi737/Portfolio.git/static/media/code-640.7f7ba521.jpg"
+  },
+  {
+    "revision": "50ddaf8b297d1c1b85feb8e585b2e6ed",
+    "url": "/adi737/Portfolio.git/static/media/html-1300.50ddaf8b.png"
+  },
+  {
+    "revision": "5b8f0c365ac53f38b2508701c277ecf4",
+    "url": "/adi737/Portfolio.git/static/media/html-300.5b8f0c36.png"
+  },
+  {
+    "revision": "4eafd5a309a11425448bda0f61ed2781",
+    "url": "/adi737/Portfolio.git/static/media/linkedin.4eafd5a3.svg"
+  },
+  {
+    "revision": "76228491f798646acd14226db367ade5",
+    "url": "/adi737/Portfolio.git/static/media/react-120.76228491.png"
+  },
+  {
+    "revision": "e467fe800cb3a4785bc973646de5f8f3",
+    "url": "/adi737/Portfolio.git/static/media/react-500.e467fe80.png"
+  }
+]);
